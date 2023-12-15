2ba2b3edb194c7745efa451b3145b823f7672abb
saemeeeee <95856651+saemeeeee@users.noreply.github.com>
2023-12-15 17:34:14 +0900

    1215

--- 3_JavaScript/object/js/실습문제33.js
+++ 3_JavaScript/object/js/실습문제33.js
@@ -1,7 +1,22 @@
-function btn1() {
-  const div = document.querySelector("#bton");
-  bton.style.color = pink;
-  bton.style.backgroundColor = pink;
-}
+const images = document.querySelectorAll(".images img");
+const span = document.querySelector("#click span");
+let count = 0;
 
-function btn2() {}
+function clickHandler() {
+  span.innerHTML = ++count;
+  const random = [
+    Math.floor(Math.random() * 3) + 1,
+    Math.floor(Math.random() * 3) + 1,
+    Math.floor(Math.random() * 3) + 1,
+  ];
+  for (let i = 0; i < images.length; i++) {
+    console.log(random[i]);
+    images[i].setAttribute("src", `../../02_CSS/resources/spy${random[i]}.jpg`);
+  }
+  if (random[0] === random[1] && random[1] === random[2]) {
+    console.log("일치!");
+    result.innerHTML = "Congratulation!! Press restart to play again~!!";
+    click.setAttribute("disabled", "disabled");
+  }
+}
+click.addEventListener("click", clickHandler);
