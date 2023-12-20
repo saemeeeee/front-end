76d55e60c86b11ad727052b24a59ef9750e3ec79
saemeeeee <95856651+saemeeeee@users.noreply.github.com>
2023-12-20 11:44:58 +0900

    1220

--- coupang/script.js
+++ coupang/script.js
@@ -12,29 +12,29 @@ navIcons[1].addEventListener("click", function () {
   });
 });
 
-// category
-const categoryBtn = document.querySelector(".category-btn");
-const category = document.querySelector(".category");
+// main
+const mainImg = document.querySelectorAll("main > img");
+// 첫번째 이미지에 main-img-active 클래스가 추가되어 있어요!
+setInterval(() => {
+  for (let i = 0; i < mainImg.length; i++) {
+    // 마지막 이미지에서는 클래스 제거, 첫번째 이미지에 클래스 추가
 
-categoryBtn.addEventListener("mouseenter", function () {
-  category.style.display = "flex";
-});
-// categoryBtn.addEventListener("mouseleae", function () {
-//   category.style.display = "none";
-// });
-category.addEventListener("mouseleave", function () {
-  category.style.display = "none";
-});
-const categoryItems = document.querySelectorAll(".category-item");
-const categorySub = document.querySelector(".category-sub");
-const categorySubItems = document.querySelectorAll(".category-sub-item");
+    if (
+      i === mainImg.length - 1 &&
+      mainImg[i].classList.contains("main-imng-active")
+    ) {
+      mainImg[i].classList.remove("main-img-active");
+      mainImg[0].classList.add("margin-img-active");
+      break;
+    }
 
-categoryItems.forEach((item, index) => {
-  item.addEventListener("mouseenter", function () {
-    categorySub.style.display = "block";
-    categorySubItems[index].style.display = "block";
-  });
-  item.addEventListener("mouseleave", function () {
-    categorySubItems[index].style.display = "none";
-  });
+    // 첫번째 이미지에서는 클래스 제거, 두번째 이미지에 클래스 추가
+    // 두번째 이미지에서는 클래스 제거, 세번째 이미지에 클래스 추가
+
+    if (mainImg[i].classList.contains("main-img-active")) {
+      mainImg[i].classList.remove("main-img-active");
+      mainImg[i + 1].classList.add("main-img-active");
+      break;
+    }
+  }
 });
